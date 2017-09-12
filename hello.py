3c561deb27680b32716c5813ad389ff5827dd2b2
romulo.mestrado <romulo.mestrado@gmail.com>
2017-09-12 15:19:44 -0400

    debugging, route with parameter and url_for

--- hello.py
+++ hello.py
@@ -1,14 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+  return url_for('show_user_profiles',username='romulomadu')
+
+@app.route('/username/<username>')
+def show_user_profiles(username):
+  # show the user profile for that user
+  return "User {:} visited".format(username)
+
+@app.route('/hello')
 def hello_world():
-  return 'Hello World!'
+  return "Hello World"
+
+@app.route('/post/<int:pos_id>')
+def show_post(pos_id):
+  return "Post {:}".format(pos_id)
 
 if __name__ == '__main__':
   host = os.getenv('IP','0.0.0.0')
   port = int(os.getenv('PORT', 5000))
+  app.debug = True
   app.run(host=host, port=port)
   #app.run()
