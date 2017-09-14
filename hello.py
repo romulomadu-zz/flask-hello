d4f5a119fadda08980289c0265925078c4f1fdce
romulo.mestrado <romulo.mestrado@gmail.com>
2017-09-14 19:18:34 -0400

    template intro

--- hello.py
+++ hello.py
@@ -1,14 +1,12 @@
 import os
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  if request.method == 'POST':
-    return 'username is {:}'.format(request.values["username"])
-  return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
-
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+  return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
   host = os.getenv('IP','0.0.0.0')
