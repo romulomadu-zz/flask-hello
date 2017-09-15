e9150d8844f66da1bd0db33b40333661b647357f
romulo.mestrado <romulo.mestrado@gmail.com>
2017-09-15 18:35:19 -0400

    logging errors

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, url_for, request, render_template, redirect, flash, session
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
-app.config['SERVER_NAME']
+#app.config['SERVER_NAME']
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -15,6 +18,7 @@ def login():
             return redirect(url_for('welcome'))
         else:
             error = "Incorrect username and password"
+            app.logger.warning('Incorrect username and password for user (%s)', request.form.get('username'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
@@ -40,6 +44,10 @@ if __name__ == '__main__':
   port = int(os.getenv('PORT', 5000))
   app.debug = True
   app.secret_key = 's\xb4\xe7\x88H\xe9\xf8\x19\xe6\xe8=@\xcc\xeb\xa0\x84<|\xd1\xcd\x04V\x1b\xc5'
+  # logging
+  handler =  RotatingFileHandler('error.log', maxBytes=10000, backupCount=1) 
+  handler.setLevel(logging.INFO)
+  app.logger.addHandler(handler)
   app.run(host=host, port=port)
   
   #app.run()
