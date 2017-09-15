1f8d25f874d9c6e6ae9ed4532d62bf83ec821329
romulo.mestrado <romulo.mestrado@gmail.com>
2017-09-14 20:21:31 -0400

    login template

--- hello.py
+++ hello.py
@@ -3,10 +3,21 @@ from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-  return render_template('hello.html', name=name)
+@app.route('/login', methods=['GET','POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if valid_login(request.form['username'],request.form['password']):
+      return "Welcome back {:}".format(request.form['username'])
+    else:
+      error = 'Incorrect username and password'
+  return render_template('login.html', error=error)
+
+def valid_login(username,password):
+  if username == password:
+    return True
+  else:
+    return False  
 
 if __name__ == '__main__':
   host = os.getenv('IP','0.0.0.0')
